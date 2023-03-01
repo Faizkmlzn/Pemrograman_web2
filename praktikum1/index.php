92da6f2f18d4096b4e2ce07fc56e458212608d50
Faizkmlzn <faizkamaluzaman0480@gmail.com>
2023-03-01 15:47:29 +0700

    Upload praktik 1

---
+++ praktikum1/index.php
@@ -0,0 +1,46 @@
+<?php 
+
+// echo "Hello World <br>";
+// echo "Nama saya Eko <br>";
+// print_r("Eko Setiawan <br>");
+// var_dump("STT Nurul Fikri");
+
+
+//membuat variabel user
+// $nama = "Eko bastian";
+// $umur = 18;
+// $berat = 59.7;
+// $mahasiswa = true;
+
+// echo "Halo, Nama saya $nama ";
+// echo "Umurku $umur tahun ";
+// echo "dan memiliki berat badan $berat kg";
+
+//membuat variabel sistem
+
+// echo "Document root " .$_SERVER['DOCUMENT_ROOT'];
+// echo "Nama file ".$_SERVER['PHP_SELF'];
+//membuat variabel konstanta
+// define('PHI', 3.14);
+// $jari = 8;
+// $luas = PHI * $jari * $jari;
+// $keliling = 2 * PHI * $jari;
+
+// echo "Luas lingkaran dengan jari-jari $jari adalah $luas ";
+// echo "<br> Keliling lingkaran adalah $keliling";
+
+//membuat array
+$programs = ["PHP", "JavaScript", "HTML", "CSS"];
+echo $programs[0];
+echo "<br> Jumlah data sebanyak ".count($programs);
+
+$x =20;
+
+$y =12;
+
+if($y>=$x){
+
+echo $y;
+
+}
+?>
